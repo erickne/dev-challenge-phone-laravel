266ec4a486297e2acfb6af8f16041e06cdbb0152
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2024-03-09 17:35:58 -0300

    feat: Added HandleIncomingCallsController to manage incoming calls

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('access_token', \App\Http\Controllers\API\GenerateAccessTokenController::class.'@generate_token');
+Route::get('access_token', \App\Http\Controllers\API\GenerateAccessTokenController::class.'@execute');
+Route::post('handle_calls', \App\Http\Controllers\API\HandleIncomingCallsController::class.'@execute');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
