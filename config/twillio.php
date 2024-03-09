716928caca4826c4f2acaa546f4f27fb960712d3
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2024-03-09 17:35:33 -0300

    feat: Added Twilio configuration file

---
+++ config/twillio.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'accountSid' => env('TWILIO_ACCOUNT_SID'),
+    'apiKeySid' => env('TWILIO_API_KEY'),
+    'apiKeySecret' => env('TWILIO_API_SECRET'),
+    'twiMLSid' => env('TWIML_APP_SID'),
+    'phoneNumber' => env('TWILIO_PHONE_NUMBER'),
+];
